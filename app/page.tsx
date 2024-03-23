dc53aa156c8f3813cf7d264dde4093a30e119fa9
Mpdanish <danishmaliyekkal786@gmail.com>
2024-03-23 17:44:43 +0530

    finished

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape} />
       <p
         className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
       ></p>
